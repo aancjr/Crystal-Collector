21f5a983ad8543ae4bbecf4780fd812250254f5f
aancjr <45945137+aancjr@users.noreply.github.com>
2019-04-14 16:47:04 -0400

    updated HTML language to include bootstrap. Converted crystal images into buttons. Added jQ: crystal buttons return value of crystal color text, computer generated number shows.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,6 +4,9 @@
     at the end of the game, win or loss, tallies are marked and computer auto gen new number.
     user then starts over with guessing using new crystal values*/
 
+//Wait for everything to be ready on the page before starting
+    $(document).ready(function(event) {
+
 //Global variables for the game.
     var computerPick = Math.floor(Math.random() * 100) + 19;
         console.log(computerPick);
@@ -12,11 +15,35 @@
         //not sure, yet, if i will need to make a random for each colored crystal or if i can just use this.
     var wins = 0;
     var losses = 0;
-    var totalScrore = 0;
+    var totalScore = 0;
+    
+    //Display wins, losses, computer pick and total score text:
+    $('#wins').text(wins);
+    $('#losses').text(losses);
+    $('#total-score').text(totalScore);
+    $('#computer-pick').text(computerPick);
+    
+    //Reset function for the game:
+    function reset() {
+        computerPick;
+        red = Math.floor(Math.random() * 12) + 1;
+        console.log(red);
+        blue = Math.floor(Math.random() * 12) + 1;
+        console.log(blue);
+        pink = Math.floor(Math.random() * 12) + 1;
+        console.log(pink);
+        purple = Math.floor(Math.random() * 12) + 1;
+        console.log(purple);
+        totalScore = 0;
+    }
 
-//Wait for everything to be ready on the page before starting
-    $(document).ready(function()) {
+    $(".crystal").on("click", function() {
+        console.log($(this).val());
+
+
+    })
+        
 
 
 
-    }   
+    })   
