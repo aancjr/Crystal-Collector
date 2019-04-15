8fa359ebfc9da680fb057d334f17ae4fcd111a32
aancjr <45945137+aancjr@users.noreply.github.com>
2019-04-15 15:17:35 -0400

    overall game play functionality complete, except for wins. wins need another gem click to reset game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,67 +5,111 @@
     user then starts over with guessing using new crystal values*/
 
 //Wait for everything to be ready on the page before starting
-    $(document).ready(function(event) {
-
-//Global variables for the game.
+//Reset function for the game:
+$(document).ready(function(event) {
+    
+    //Global variables for the game.
     var computerPick = Math.floor(Math.random() * 100) + 19;
-        console.log(computerPick);
-    //var crystal = Math.floor(Math.random() * 12) + 1;
-      //  console.log(crystal);
-        //not sure, yet, if i will need to make a random for each colored crystal or if i can just use this.
+        $('#computer-pick').text(computerPick);
     var red = parseInt(Math.floor(Math.random() * 12) + 1);
     var blue = parseInt(Math.floor(Math.random() * 12) + 1);
     var pink = parseInt(Math.floor(Math.random() * 12) + 1);
     var purple = parseInt(Math.floor(Math.random() * 12) + 1);
-
+    
     var wins = 0;
     var losses = 0;
     var totalScore = 0;
     
-    //Display wins, losses, computer pick and total score text:
-    $('#wins').text(wins);
-    $('#losses').text(losses);
-    $('#total-score').text(totalScore);
-    $('#computer-pick').text(computerPick);
-    
-    //Reset function for the game:
-    function reset() {
-        computerPick;
-        red;
-        blue;
-        pink;
-        purple;
+    function resetGame() {
+        computerPick = Math.floor(Math.random() * 100) + 19;
+            $('#computer-pick').text(computerPick);
         totalScore = 0;
+            $('#total-score').text(totalScore);
+        red = parseInt(Math.floor(Math.random() * 12) + 1);
+        blue = parseInt(Math.floor(Math.random() * 12) + 1);
+        pink = parseInt(Math.floor(Math.random() * 12) + 1);
+        purple = parseInt(Math.floor(Math.random() * 12) + 1);
     }
-
-    $("#button-red").on("click", function() {
-        console.log(red)
-        totalScore = red + totalScore;
-        $('#total-score').text(totalScore);
-        console.log(totalScore);
-
-    })
-
-    $("#button-blue").on("click", function() {
-        console.log(blue)
-        totalScore = blue + totalScore;
-        $('#total-score').text(totalScore);
-
-    })
-
-    $("#button-pink").on("click", function() {
-        console.log(pink)
-        totalScore = pink + totalScore;
-        $('#total-score').text(totalScore);
-
-    })
-
-    $("#button-purple").on("click", function() {
-        console.log(purple)
-        totalScore = purple + totalScore;
-        $('#total-score').text(totalScore);
-
-    })
     
+        $("#button-red").on("click", function() {
+            
+            if (totalScore === computerPick) {
+                wins++;
+                    $("#wins").text(wins);
+                resetGame ();
+            }
+            
+            if (totalScore > computerPick) {
+                losses++;
+                    $("#losses").text(losses);
+                resetGame ();
+            }
+            
+            console.log(red)
+            totalScore += red;
+                $('#total-score').text(totalScore);
+        })
+        
+        $("#button-blue").on("click", function() {
+            console.log(blue)
+            totalScore += blue;
+                $('#total-score').text(totalScore);
 
+            if (totalScore === computerPick) {
+                wins++;
+                    $("#wins").text(wins);
+                resetGame ();
+            }
+            
+            if (totalScore > computerPick) {
+                losses++;
+                    $("#losses").text(losses);
+                resetGame ();
+            }
+
+        })
+        
+        $("#button-pink").on("click", function() {
+            console.log(pink)
+            totalScore += pink;
+                $('#total-score').text(totalScore);
+
+            if (totalScore === computerPick) {
+                wins++;
+                    $("#wins").text(wins);
+                resetGame ();
+            }
+            
+            if (totalScore > computerPick) {
+                losses++;
+                    $("#losses").text(losses);
+                resetGame ();
+            }
+
+        })
+        
+        $("#button-purple").on("click", function() {
+            console.log(purple)
+            totalScore += purple;
+                $('#total-score').text(totalScore);
+            
+            if (totalScore === computerPick) {
+                wins++;
+                    $("#wins").text(wins);
+                resetGame ();
+            }
+            
+            if (totalScore > computerPick) {
+                losses++;
+                    $("#losses").text(losses);
+                resetGame ();
+            }
+        })
+        
+    
+       /* $("#losses").text(losses);
+        $('#wins').text(wins);
+        $('#total-score').text(totalScore);*/
+        
     })   
+    
